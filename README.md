63f5e00adcece025d092a82b57c5007add430608
EC2 Default User <ec2-user@ip-172-31-48-179.us-west-2.compute.internal>
2019-10-30 02:46:16 +0000

    index page test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hw1
+# mikie Reed
+# CST336
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<!--
+by: mikie Reed
+Date: 10.29.19
+CST 336
+-->
+    <head>
+        <meta charset="utf-8" />
+        <title> Operating Systems </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header>
+           <h1> Operating Systems </h1>
+        </header>
+        <hr />
+        <nav>
+           <a href="index.html" id="current">Home</a>
+           <a href="windows.html">Windows</a>
+           <a href="mac.html">Mac OS</a>
+           <a href="linux.html">Linux</a>
+        </nav>
+        <br /><br />
+        <main>
+           <figure>
+              <img src="img/motherboard.jpg" alt="Picture of a Motherboard" />
+           </figure>
+           <div>
+              <h2>What is an "Operating System"?</h2>
+              <p>The operating system is what runs a computer. It is the 
+                 operating system that decides what happens, what runs, 
+                 and what gets the resources.</p>
+              <hr>
+              <h2>What are the main Operating Systems?</h2>
+              <p>There are three main operating systems: Windows, Mac OS,
+                  and Linux.</p>
+           </div>
+        </main>
+        <footer>
+            <hr>
+            CST336 Internet Programming. 2019&copy; mikie Reed <br />
+            <img src="img/csumb_logo.png" alt="CSUMB Logo" />
+        </footer>
+    </body>
+</html>
